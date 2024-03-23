c946e53745066a5080c52534198739cedd8b07d5
El レイ <144611397+eielelle@users.noreply.github.com>
2024-03-23 10:32:59 +0800

    Add files via upload

---
+++ CaclulatorPlus/Calculator.cs
@@ -0,0 +1,385 @@
+﻿using CaclulatorPlus;
+using System;
+using System.Collections.Generic;
+
+namespace CalculatorPlus
+{
+    class Calculator
+    {
+        private static string expression = "0";
+        private static string infix = "";
+        private static string preview = "";
+        private static bool isRecentlyCalc = false;
+        private static bool isRecentlyFunc = false;
+
+        public static void Sin()
+        {
+            isRecentlyFunc = true;
+            infix += $"{Math.Sin(double.Parse(expression))} ";
+            preview += $"sin({expression}) ";
+        }
+
+        // cos
+        public static void Cos()
+        {
+            isRecentlyFunc = true;
+            infix += $"{Math.Cos(double.Parse(expression))} ";
+            preview += $"cos({expression}) ";
+        }
+
+        // tan
+        public static void Tan()
+        {
+            isRecentlyFunc = true;
+            infix += $"{Math.Tan(double.Parse(expression))} ";
+            preview += $"tan({expression}) ";
+        }
+
+        // log
+        public static void Log()
+        {
+            isRecentlyFunc = true;
+            infix += $"{Math.Log(double.Parse(expression))} ";
+            preview += $"log({expression}) ";
+        }
+
+        // pi
+        public static void PI()
+        {
+            infix += $"{Math.PI} ";
+            expression = Math.PI.ToString();
+        }
+
+        // square root
+        public static void Sqrt()
+        {
+            isRecentlyFunc = true;
+            infix += $"{Math.Sqrt(double.Parse(expression))} ";
+            preview += $"sqrt({expression}) ";
+        }
+
+        // x2
+        public static void Squared()
+        {
+            isRecentlyFunc = true;
+            infix += $"{Math.Pow(double.Parse(expression), 2)} ";
+            preview += $"{expression} ^ 2 ";
+        }
+
+        // exp
+        public static void Exp()
+        {
+            isRecentlyFunc = true;
+            infix += $"{Math.Exp(double.Parse(expression))} ";
+            preview += $"exp({expression}) ";
+        }
+
+        public static void Round()
+        {
+            isRecentlyFunc = true;
+            infix += $"{Math.Round(double.Parse(expression))} ";
+            preview += $"round({expression}) ";
+        }
+
+        // xy
+        public static void Raise()
+        {
+            if (isRecentlyFunc)
+            {
+
+                infix += $"{expression} ^ ";
+                preview += $"{expression} ^ ";
+
+                isRecentlyFunc = false;
+                return;
+            }
+
+            infix += "^ ";
+            preview += "^ ";
+        }
+
+        // factorial
+        public static void Factorial()
+        {
+            isRecentlyFunc = true;
+
+            double result = 1;
+            for (double i = 1; i <= double.Parse(expression); i++)
+            {
+                result *= i;
+            }
+
+            infix += $"{result} ";
+            preview += $"{expression}! ";
+        }
+
+        public static void Mod()
+        {
+            if (isRecentlyFunc)
+            {
+                infix += $"{expression} % ";
+                preview += $"{expression} Mod ";
+
+                isRecentlyFunc = false;
+                return;
+            }
+
+            infix += "% ";
+            preview += "Mod ";
+        }
+
+        public static void Add()
+        {
+            if (isRecentlyFunc)
+            {
+                infix += "+ ";
+                preview += "+ ";
+
+                isRecentlyFunc = false;
+                return;
+            }
+
+            infix += $"{expression} + ";
+            preview += $"{expression} + ";
+        }
+
+        public static void Subtract()
+        {
+            if (isRecentlyFunc)
+            {
+                infix += "- ";
+                preview += "- ";
+
+                isRecentlyFunc = false;
+                return;
+            }
+
+            infix += $"{expression} - ";
+            preview += $"{expression} - ";
+        }
+
+        public static void Multiply()
+        {
+            if (isRecentlyFunc)
+            {
+                infix += "* ";
+                preview += "× ";
+
+                isRecentlyFunc = false;
+                return;
+            }
+
+            infix += $"{expression} * ";
+            preview += $"{expression} × ";
+        }
+
+        public static void Divide()
+        {
+            if (isRecentlyFunc)
+            {
+                infix += "/ ";
+                preview += "÷ ";
+
+                isRecentlyFunc = false;
+                return;
+            }
+
+            infix += $"{expression} / ";
+            preview += $"{expression} ÷ ";
+        }
+
+        public static void InsertDecimalPoint()
+        {
+            if (!expression.Contains('.'))
+            {
+                expression += ".";
+            }
+        }
+
+        public static double Equals() {
+            infix += expression;
+            preview += expression;
+            double result = Calculate();
+
+            return result;
+        }
+
+        public static void SetExpression(double val)
+        {
+            double currentValue = double.Parse(expression);
+
+            if ((currentValue != 0 || expression.Contains('.')) && !isRecentlyCalc)
+            {
+                expression += val;
+            }
+            else
+            {
+                expression = val.ToString();
+                isRecentlyCalc = false;
+            }
+        }
+
+        public static void Remove()
+        {
+            if (expression.Length > 1)
+            {
+                expression = expression.Remove(expression.Length - 1, 1);
+            }
+            else
+            {
+                Reset();
+            }
+        }
+
+        public static void Clear()
+        {
+            preview = "";
+            infix = "";
+        }
+
+        public static void Reset()
+        {
+            expression = "0";
+            infix = "";
+            preview = "";
+        }
+
+        public static string GetExpression()
+        {
+            return expression;
+        }
+
+        public static string GetPreview()
+        {
+            return preview;
+        }
+
+        private static string ParseInfix(string infix)
+        {
+            int i = infix.Length - 1;
+
+            // Trim ends of infix
+            while (i >= 0 && !char.IsDigit(infix[i]))
+            {
+                if (char.IsWhiteSpace(infix[i]) || IsOperator(infix[i].ToString()))
+                {
+                    i--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            // Split into an array
+            return infix[..(i + 1)];
+        }
+
+        private static string ToPostFix(string infix)
+        {
+            string postfix = "";
+            string[] infixArr = ParseInfix(infix).Split(' ');
+            Stack<char> stack = new();
+
+            foreach (string token in infixArr)
+            {
+                if (IsOperand(token))
+                {
+                    postfix += $"{token} ";
+                }
+
+                if (IsOperator(token))
+                {
+                    char c = Convert.ToChar(token);
+
+                    if (stack.Count > 0 && GetOperatorPrecedence(c) <= GetOperatorPrecedence(stack.Peek()))
+                    {
+                        postfix += $"{stack.Pop()} ";
+                    }
+
+                    stack.Push(c);
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                postfix += $"{stack.Pop()} ";
+            }
+
+            return postfix;
+        }
+
+        public static double Calculate()
+        {
+            isRecentlyCalc = true;
+
+            string[] postfix = ToPostFix(infix).Split(" ");
+            Stack<string> stack = new();
+
+            foreach (string token in postfix)
+            {
+                if (IsOperand(token))
+                {
+                    stack.Push(token);
+                }
+
+                if (IsOperator(token))
+                {
+                    double a = double.Parse(stack.Pop());
+                    double b = double.Parse(stack.Pop());
+
+                    switch(token)
+                    {
+                        case "+":
+                            stack.Push(Convert.ToString(b + a));
+                            break;
+                        case "-":
+                            stack.Push(Convert.ToString(b - a));
+                            break;
+                        case "*":
+                            stack.Push(Convert.ToString(b * a));
+                            break;
+                        case "/":
+                            stack.Push(Convert.ToString(b / a));
+                            break;
+                        case "%":
+                            stack.Push(Convert.ToString(b % a));
+                            break;
+                        case "^":
+                            stack.Push(Convert.ToString(Math.Pow(b, a)));
+                            break;
+
+                    }
+                }
+            }
+
+            if (stack.Count > 0)
+            {
+                expression = stack.Pop();
+            }
+
+            return double.Parse(expression);
+        }
+
+        private static bool IsOperator(string op)
+        {
+            return char.TryParse(op, out char c) && (c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '^');
+        }
+
+        private static bool IsOperand(string operand)
+        {
+            return double.TryParse(operand, out _);
+        }
+
+        private static int GetOperatorPrecedence(char op)
+        {
+            return op switch
+            {
+                '+' or '-' => 1,
+                '*' or '/' or '%' => 2,
+                '^' => 3,
+                _ => 0,
+            };
+        }
+    }
+}
---
+++ CaclulatorPlus/Form1.cs
@@ -0,0 +1,297 @@
+using CalculatorPlus;
+
+namespace CaclulatorPlus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Seven_Click(object sender, EventArgs e)
+        {
+            Calculator.SetExpression(7);
+            this.currentValue.Text = Calculator.GetExpression();
+        }
+
+        private void Five_Click(object sender, EventArgs e)
+        {
+            Calculator.SetExpression(5);
+            this.currentValue.Text = Calculator.GetExpression();
+        }
+
+        private void Eight_Click(object sender, EventArgs e)
+        {
+            Calculator.SetExpression(8);
+            this.currentValue.Text = Calculator.GetExpression();
+        }
+
+        private void Nine_Click(object sender, EventArgs e)
+        {
+            Calculator.SetExpression(9);
+            this.currentValue.Text = Calculator.GetExpression();
+        }
+
+        private void Six_Click(object sender, EventArgs e)
+        {
+            Calculator.SetExpression(6);
+            this.currentValue.Text = Calculator.GetExpression();
+        }
+
+        private void Three_Click(object sender, EventArgs e)
+        {
+            Calculator.SetExpression(3);
+            this.currentValue.Text = Calculator.GetExpression();
+        }
+
+        private void Two_Click(object sender, EventArgs e)
+        {
+            Calculator.SetExpression(2);
+            this.currentValue.Text = Calculator.GetExpression();
+        }
+
+        private void One_Click(object sender, EventArgs e)
+        {
+            Calculator.SetExpression(1);
+            this.currentValue.Text = Calculator.GetExpression();
+        }
+
+        private void Four_Click(object sender, EventArgs e)
+        {
+            Calculator.SetExpression(4);
+            this.currentValue.Text = Calculator.GetExpression();
+        }
+
+        private void Zero_Click(object sender, EventArgs e)
+        {
+            Calculator.SetExpression(0);
+            this.currentValue.Text = Calculator.GetExpression();
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            Calculator.Remove();
+            this.currentValue.Text = Calculator.GetExpression();
+        }
+
+        private void ClearAll_Click(object sender, EventArgs e)
+        {
+            Calculator.Reset();
+            this.currentValue.Text = Calculator.GetExpression();
+            this.expressionPreview.Text = Calculator.GetPreview();
+        }
+
+        private void Dot_Click(object sender, EventArgs e)
+        {
+            Calculator.InsertDecimalPoint();
+            this.currentValue.Text = Calculator.GetExpression();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            Calculator.Add();
+            this.expressionPreview.Text = Calculator.GetPreview();
+            this.currentValue.Text = Calculator.Calculate().ToString();
+        }
+
+        private void Substract_Click(object sender, EventArgs e)
+        {
+            Calculator.Subtract();
+            this.expressionPreview.Text = Calculator.GetPreview();
+            this.currentValue.Text = Calculator.Calculate().ToString();
+        }
+
+        private void Multiply_Click(object sender, EventArgs e)
+        {
+            Calculator.Multiply();
+            this.expressionPreview.Text = Calculator.GetPreview();
+            this.currentValue.Text = Calculator.Calculate().ToString();
+        }
+
+        private void Divide_Click(object sender, EventArgs e)
+        {
+            Calculator.Divide();
+            this.expressionPreview.Text = Calculator.GetPreview();
+            this.currentValue.Text = Calculator.Calculate().ToString();
+        }
+
+        private void Equals_Click(object sender, EventArgs e)
+        {
+            string result = Calculator.Equals().ToString();
+            string preview = Calculator.GetPreview();
+            Calculator.Clear();
+
+            this.currentValue.Text = result;
+            this.expressionPreview.Text = Calculator.GetPreview();
+            this.history.Text += $"{preview} = {result}\n";
+        }
+
+        private void Sin_Click(object sender, EventArgs e)
+        {
+            Calculator.Sin();
+            this.expressionPreview.Text = Calculator.GetPreview();
+            this.currentValue.Text = Calculator.Calculate().ToString();
+        }
+
+        private void Cos_Click(object sender, EventArgs e)
+        {
+            Calculator.Cos();
+            this.expressionPreview.Text = Calculator.GetPreview();
+            this.currentValue.Text = Calculator.Calculate().ToString();
+        }
+
+        private void Tan_Click(object sender, EventArgs e)
+        {
+            Calculator.Tan();
+            this.expressionPreview.Text = Calculator.GetPreview();
+            this.currentValue.Text = Calculator.Calculate().ToString();
+        }
+
+        private void Log_Click(object sender, EventArgs e)
+        {
+            Calculator.Log();
+            this.expressionPreview.Text = Calculator.GetPreview();
+            this.currentValue.Text = Calculator.Calculate().ToString();
+        }
+
+        private void Mod_Click(object sender, EventArgs e)
+        {
+            Calculator.Mod();
+            this.expressionPreview.Text = Calculator.GetPreview();
+            this.currentValue.Text = Calculator.Calculate().ToString();
+        }
+
+        private void Pi_Click(object sender, EventArgs e)
+        {
+            Calculator.PI();
+            this.expressionPreview.Text = Calculator.GetPreview();
+            this.currentValue.Text = Calculator.GetExpression();
+        }
+
+        private void Root_Click(object sender, EventArgs e)
+        {
+            Calculator.Sqrt();
+            this.expressionPreview.Text = Calculator.GetPreview();
+            this.currentValue.Text = Calculator.Calculate().ToString();
+        }
+
+        private void RaiseTwo_Click(object sender, EventArgs e)
+        {
+            Calculator.Squared();
+            this.expressionPreview.Text = Calculator.GetPreview();
+            this.currentValue.Text = Calculator.Calculate().ToString();
+        }
+
+        private void RaiseY_Click(object sender, EventArgs e)
+        {
+            Calculator.Raise();
+            this.expressionPreview.Text = Calculator.GetPreview();
+            this.currentValue.Text = Calculator.Calculate().ToString();
+        }
+
+        private void Factorial_Click(object sender, EventArgs e)
+        {
+            Calculator.Factorial();
+            this.expressionPreview.Text = Calculator.GetPreview();
+            this.currentValue.Text = Calculator.Calculate().ToString();
+        }
+
+        private void Exp_Click(object sender, EventArgs e)
+        {
+            Calculator.Exp();
+            this.expressionPreview.Text = Calculator.GetPreview();
+            this.currentValue.Text = Calculator.Calculate().ToString();
+        }
+
+        private void Round_Click(object sender, EventArgs e)
+        {
+            Calculator.Round();
+            this.expressionPreview.Text = Calculator.GetPreview();
+            this.currentValue.Text = Calculator.Calculate().ToString();
+        }
+
+        private void ShowHistoryBtn_Click(object sender, EventArgs e)
+        {
+            if (historyPanel.Height > 0)
+            {
+                historyPanel.Size = new Size(historyPanel.Width, 0);
+                return;
+            }
+
+            historyPanel.Size = new Size(historyPanel.Width, 244);
+        }
+
+        private void ClearHistory_Click(object sender, EventArgs e)
+        {
+            this.history.Text = "";
+        }
+
+        private void Form_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char keyPressed = e.KeyChar;
+
+            switch (keyPressed)
+            {
+                case var _ when char.IsDigit(keyPressed):
+                    // Handle digit key press
+                    Calculator.SetExpression(double.Parse(keyPressed.ToString()));
+                    this.currentValue.Text = Calculator.GetExpression();
+                    break;
+                case '.':
+                    // Handle decimal point key press
+                    Dot_Click(sender, e);
+                    break;
+                case '+':
+                    // Handle operator key press
+                    Add_Click(sender, e);
+                    break;
+                case '-':
+                    // Handle operator key press
+                    Substract_Click(sender, e);
+                    break;
+                case '*':
+                    // Handle operator key press
+                    Multiply_Click(sender, e);
+                    break;
+                case '/':
+                    // Handle operator key press
+                    Divide_Click(sender, e);
+                    break;
+                case '%':
+                    // Handle operator key press
+                    Mod_Click(sender, e);
+                    break;
+                case '^':
+                    // Handle operator key press
+                    Exp_Click(sender, e);
+                    break;
+                case '=':
+                    // Handle operator key press
+                    Equals_Click(sender, e);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Form_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Back)
+            {
+                // Handle Backspace key press
+                Clear_Click(sender, e);
+            }
+        }
+    }
+}
